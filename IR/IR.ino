7f43be94ca2eead6772fff64a6e40c4f5922d94b
cclngit <38783129+cclngit@users.noreply.github.com>
2021-01-23 15:46:27 +0100

    Ajoutez des fichiers projet.

---
+++ IR/IR.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		IR.ino
+ Created:	23/01/2021 15:45:25
+ Author:	cicil
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
